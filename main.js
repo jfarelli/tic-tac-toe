b7b77e5112fa6d5c5901476fc3fc47d4d1e7f3a6
Jordan Farelli <jordanfarelli@gmail.com>
2022-04-24 12:20:34 -0600

    Player wins not accumulate on the page after win

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
+var game = new Game();
+var sleuth = game.player1
+var hooligan = game.player2
+var restartButton = document.getElementById("restartButton")
+var gameBoardSquares = document.querySelectorAll('.cell')
 var playerTurnText = document.querySelector('.player-turn');
 var winningText = document.querySelector(".winning-text");
 var winningMessage = document.getElementById("winningMessage");
 var displaySleuthWins = document.querySelector('.display-sleuth-wins');
 var displayHooliganWins = document.querySelector('.display-hooligan-wins');
 
-// var playerTurn = document.getElementById('displayTurn');
-// playerTurn.innerHTML = displayPlayerTurn();
-// var playerTurnDisplay = document.querySelector('player-turn');
-
-var game = new Game();
-var sleuth = new Player();
-var hooligan = new Player();
-var restartButton = document.getElementById("restartButton")
-var gameBoardSquares = document.querySelectorAll('.cell')
-
-
-
-
 gameStart();
 
 restartButton.addEventListener('click', gameStart);
+
 
 
 function gameStart() {
@@ -39,10 +32,10 @@ function squareClicked(e) {
     var square = e.target;
     sleuth = 'sleuth';
     hooligan = 'hooligan';
-    const playerMove = game.currentPlayer ? hooligan : sleuth;
+    game.playerMove = game.currentPlayer ? hooligan : sleuth;
     displayPlayerTurn();
-    iconPlacement(square, playerMove);
-    if (whoWins(playerMove)) {
+    iconPlacement(square, game.playerMove);
+    if (whoWins(game.playerMove)) {
         gameCompleted(false);
     } 
     if (itsADraw()) {
@@ -55,8 +48,7 @@ function gameCompleted(draw) {
         winningText.innerText = `It's a stalemate!`
     } else {
         winningText.innerText = `The ${game.currentPlayer ? 'Sleuth' : 'Hooligan'} wins!`
-        // return game.currentPlayer.wincount();
-        // gam.currentPlayer.increaseWins()
+        game.updatePlayerWins()
     }
     winningMessage.classList.add('show');
 }
@@ -67,60 +59,32 @@ function itsADraw() {
     })
 }
 
-function iconPlacement(square, playerMove) {
-    square.classList.add(playerMove)
+function iconPlacement(square) {
+    square.classList.add(game.playerMove)
 }
 
-function whoWins(playerMove) {
+function whoWins() {
     return game.winningNumbers.some(combination => {
         return combination.every(square => {
-            return gameBoardSquares[square].classList.contains(playerMove)
+            return gameBoardSquares[square].classList.contains(game.playerMove)
         })
     }) 
 }
 
-// function displayWins() {
-//     displaySleuthWins.innerText = (`${game.player1.winCount} WINS`);
-//     displayHooliganhWins.innerText = (`${game.player2.winCount} WINS`);
-//   }
-
-// //need to stop display after win
 function displayPlayerTurn() {
     game.whosTurn()
     playerTurnText.innerText = `It's the ${game.currentPlayer ? 'Hooligan' : 'Sleuth'}'s turn!`
 }
 
-// should invoke increaseWins()
-// should be displayed on the screen
-// should be updated every win
-// function displayPlayerWins() {
-//     if (game.currentPlayer === sleuth) {
-//         sleuth.increaseWins()
-//         displayWinCount.innerText = `${sleuth}`;
-//     } else if (game.currentPlayer === hooligan) {
-//         hooligan.increaseWins()
-//         displayWinCount.innerText = `${hooligan}`;
-//     }
-// }
-
 function displayWinCount() {
     if (whoWins(game.currentPlayer)) {
-        return displaySleuthWins.innerText = `${game.player1.winCount}`
+        displaySleuthWins.innerText = `${game.player1.wins}`
     } else {
-        return displayHooliganWins.innerText = `${game.player2.winCount}`
+        displayHooliganWins.innerText = `${game.player2.wins}`
   }
 }
-// function displayPlayerTurn(playerMove) {
-//     // game.currentPlayer = `It's the ${game.currentPlayer}'s turn!`
-//     if (whoWins(playerMove)) {
-//         return playerMove.playerTurn.innerHTML = `It's ${game.currentPlayer}'s Turn!`;
-//     }
-//     // if (game.currentPlayer === 'sleuth') {
-//     //     return playerTurn.innerHTML = `It's ${game.currentPlayer}'s Turn!`;
-//     // } else if (game.currentPlayer === 'hooligan') {
-//     //     return playerTurn.innerHTML = `It's ${game.currentPlayer}'s Turn!`;
-//     // }
-// }
+
+
 
 
 
